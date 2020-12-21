4a865ba82c6dacbd33356f1e9aaf5fee0ec74bdf
iulover99 <iulover99@gmail.com>
2020-12-21 20:29:21 +0700

    Change the purpose of the website to make it easier to trick people

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Route, useHistory} from 'react-router-dom'
 import './App.css'
 import AlarmManagerScreen from './components/AlarmManagerScreen'
 import MessageScreen from './components/MessageScreen'
+import UselessScreen from './components/UselessScreen'
 
 
 function App() {
@@ -14,8 +15,10 @@ function App() {
       history.push(`/message/${keyword}`)
     } else if(keyword=== "luonvuituoi" ){
       history.push("/qgql27cqKE4gHkMDYsob")
+    } else if(keyword=== "quanbidien"){
+      history.push("/FlbSpuouohaaylukvp")
     } else {
-      window.alert("Your opinion has been send")
+      window.alert("Our services is down at the moment due to some unexpected issues, please try again later...")
       history.push("/")
     }
 
@@ -23,22 +26,23 @@ function App() {
   return (
       <div className="App grid-container">
         <header className="App-header">
-          <h1 className="line-1 anim-typewriter">Share your anonymous thought for everyone to see...</h1>
+          <h1 className="line-1 anim-typewriter">Shorten your url to become a lucky person and gain our prize!</h1>
         </header>
         <main className="main">
-          <form onSubmit={handleClick} className="keyword-bar">
+          <form onSubmit={handleClick} className="keyword-bar" autoComplete="off">
             <input 
               type="text" 
               name="keyword-input" 
               id="keyword-input" 
               onChange= {({ target }) => setKeyword(target.value)}
-              placeholder="Giàu vì bạn, sang vì vợ..."
+              placeholder="Put your link here..."
             />
-            <button type="submit"> Send </button>
+            <button type="submit"> Confirm </button>
           </form>
           <section className="content">
             <Route path="/message/:code" component={MessageScreen} />
             <Route path="/qgql27cqKE4gHkMDYsob" exact={true} component={AlarmManagerScreen} />
+            <Route path="/FlbSpuouohaaylukvp" exact={true} component={UselessScreen} />   {/* shift 7 */}
           </section>
         </main>
         <footer className="footer">
---
+++ src/components/UselessScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const UselessScreen = () =>{
+    return(
+        <div className="useless-shit">
+            <div class="heart">&#x2665;</div>
+            <h1>Yêu Linh!!!</h1>
+        </div>
+    )
+}
+
+export default UselessScreen
